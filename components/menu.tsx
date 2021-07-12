6b10eb2be4cbc70ece0a1c37982c567e390fa619
Saga Kortesaari <saga.kortesaari@hotmail.com>
2021-07-13 00:24:02 +0200

    Global component for the menu across the whole site

---
+++ components/menu.tsx
@@ -0,0 +1,9 @@
+interface MenuBarProps {
+    // TBA
+}
+
+export default function MenuBar({}) {
+    return (
+        <h1> This will be the global MenuBar </h1>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import MenuBar from '../components/menu'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <MenuBar />
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp
