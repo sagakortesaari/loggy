f02f5520135017f4676b495fd847b78140e833c3
Saga Kortesaari <saga.kortesaari@hotmail.com>
2021-07-19 19:25:12 +0200

    aligned menu + added main page info div

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react"
-import styles from '@styles/menu.module.css'
-import Link from 'next/link'
+import React, { useState } from "react";
+import styles from "@styles/menu.module.css";
+import Link from "next/link";
 
 export const MenuBar = () => {
-    const [mode, setMode] = useState()
-    
-    return (
-        <>
-            <div className={styles.menubar}>
-                <div className={styles.flex}>
-                    <div id={styles.menuname}> sk. </div>
-                    <div className={styles.linkDiv}>
-                        <Link href="/blog">
-                            <a className={styles.links}>blog</a>
-                        </Link>
-                        <Link href="/about">
-                            <a className={styles.links}>about me</a>
-                        </Link>
-                        <div id={styles.toggleMode}> 🌙 </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const [mode, setMode] = useState();
+
+  return (
+    <>
+      <div className={styles.menubar}>
+        <div className={styles.flex}>
+          <div id={styles.menuname}> sk. </div>
+          <div className={styles.linkDiv}>
+            <Link href="/blog">
+              <a className={styles.links}>blog</a>
+            </Link>
+            <Link href="/about">
+              <a className={styles.links}>about me</a>
+            </Link>
+            <div id={styles.toggleMode}> 🌙 </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
