3a36ad87ac59d8486dd389e03fca141f53ca6cae
Saga Kortesaari <saga.kortesaari@hotmail.com>
2021-07-19 16:54:37 +0200

    edited global page header + added new pages

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,9 +1,26 @@
-interface MenuBarProps {
-    // TBA
-}
+import React, { useState } from "react"
+import styles from '@styles/menu.module.css'
+import Link from 'next/link'
 
-export default function MenuBar({}) {
+export const MenuBar = () => {
+    const [mode, setMode] = useState()
+    
     return (
-        <h1> This will be the global MenuBar </h1>
+        <>
+            <div className={styles.menubar}>
+                <div className={styles.flex}>
+                    <div id={styles.menuname}> sk. </div>
+                    <div className={styles.linkDiv}>
+                        <Link href="/blog">
+                            <a className={styles.links}>blog</a>
+                        </Link>
+                        <Link href="/about">
+                            <a className={styles.links}>about me</a>
+                        </Link>
+                        <div id={styles.toggleMode}> 🌙 </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import MenuBar from '../components/menu'
+import { MenuBar } from "@components/menu";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <MenuBar />
     <Component {...pageProps} />
   </>
 }
-export default MyApp
