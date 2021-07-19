16c958bb9f5070a4d69c72be42462a164124ce2a
Saga Kortesaari <saga.kortesaari@hotmail.com>
2021-07-19 21:41:34 +0200

    added text + photo to index header

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,20 @@ import styles from "@styles/index.module.css";
 import profilePic from "@assets/me.jpg";
 
 export default function Home() {
+  // Todo: Add global stylesheet and use e.g. fonts from that so I don't have to re-write these CSS classes.
   return (
     <div className={styles.container}>
-      <div>
-        Hi, i'm saga!
-        <Image src={profilePic}/>
+      <div className={styles.header}>
+        <div className={styles.headertext}>
+          <p className={styles.indexheader}>
+            hi, i’m saga! 👋🏻
+          </p>
+          <p className={styles.indexparagraph}>
+            this website is intended to be somewhat of a digital
+            notebook. i write about all sorts of stuff - but mostly about tech + engineering.
+          </p>
+        </div>
+        <Image className={styles.headerimage} width={300} height={300} src={profilePic}/>
       </div>
     </div>
   );
